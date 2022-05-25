98a5f6779f70d30f2c558ea875aa695f09d64eb3
Leon Molina <leon.maguiar@gmail.com>
2022-05-25 16:38:50 -0300

    feat: added agent card base structure and style

---
+++ components/CollaboratorCard/index.tsx
@@ -0,0 +1,31 @@
+import { AgentAvatar, AgentItemArea, AgentItemText, AgentMainArea, AgentName, AgentOptions, AgentStatus, AgentStatusText, Container } from "./styles";
+
+export const CollaboratorCard = () => {
+  return (
+    <Container>
+      <AgentMainArea>
+        <AgentAvatar
+          src="https://github.com/leonmolina.png"
+        />
+        <AgentName>Leon Molina Aguiar</AgentName>
+      </AgentMainArea>
+      {/* Departamento */}
+      <AgentItemArea>
+        <AgentItemText>Administrativo</AgentItemText>
+      </AgentItemArea>
+      {/* Cargo */}
+      <AgentItemArea>
+        <AgentItemText>Diretor</AgentItemText>
+      </AgentItemArea>
+      {/* Unidade */}
+      <AgentItemArea>
+        <AgentItemText>Quartel General</AgentItemText>
+      </AgentItemArea>
+      {/* Status */}
+      <AgentStatus>
+        <AgentStatusText>Ativo</AgentStatusText>
+      </AgentStatus>
+      <AgentOptions>...</AgentOptions>
+    </Container>
+  );
+}
---
+++ components/CollaboratorCard/styles.ts
@@ -0,0 +1,22 @@
+import styled from "@emotion/styled";
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+`
+export const AgentMainArea = styled.div`
+`
+export const AgentAvatar = styled.img`
+`
+export const AgentName = styled.span`
+`
+export const AgentItemArea = styled.div`
+`
+export const AgentItemText = styled.span`
+`
+export const AgentStatus = styled.div`
+`
+export const AgentStatusText = styled.span`
+`
+export const AgentOptions = styled.div`
+`
