378c9f496dd120494c81a23da9b51010115c72bb
Leon Molina <leon.maguiar@gmail.com>
2022-05-25 00:40:50 -0300

    added emotion styled

--- components/Organization/index.tsx
+++ components/Organization/index.tsx
@@ -1,12 +1,13 @@
 import { Collaborators } from "../Collaborators"
 import { Roles } from "../Roles"
+import { Container } from "./styles"
 
 export const Organization = () => {
   return (
-    <div>
+    <Container>
       <h3>Colaboradores | Cargos</h3>
       <Collaborators />
       <Roles />
-    </div>
+    </Container>
   )
 }
