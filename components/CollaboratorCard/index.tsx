2fa173043a7fbb468125c518c60193582d0de92e
Leon Molina <leon.maguiar@gmail.com>
2022-05-27 20:34:13 -0300

    refactor: card main area style

--- components/CollaboratorCard/index.tsx
+++ components/CollaboratorCard/index.tsx
@@ -9,6 +9,8 @@ import {
   AgentStatus,
   AgentStatusText,
   Container,
+  MainAreaLeftSide,
+  MainAreaRightSide,
 } from "./styles";
 
 type Props = {
@@ -17,40 +19,44 @@ type Props = {
 
 export const CollaboratorCard = ({ agents }: Props) => {
   return (
-      <Container>
-        <AgentItems className="row">
-          <AgentMainArea className="col-3">
+    <Container>
+      <AgentItems className="row">
+        <AgentMainArea className="col-3">
+          <MainAreaLeftSide>
             <AgentAvatar src={agents.image} />
+          </MainAreaLeftSide>
+          <MainAreaRightSide>
             <AgentItemText>{agents.name}</AgentItemText>
-          </AgentMainArea>
+          </MainAreaRightSide>
+        </AgentMainArea>
 
-          {/* Departamento */}
-          <AgentItemArea className="col-2">
-            <AgentItemText>{agents.department}</AgentItemText>
-          </AgentItemArea>
+        {/* Departamento */}
+        <AgentItemArea className="col-2">
+          <AgentItemText>{agents.department}</AgentItemText>
+        </AgentItemArea>
 
-          {/* Cargo */}
-          <AgentItemArea className="col-2">
-            <AgentItemText>{agents.role}</AgentItemText>
-          </AgentItemArea>
+        {/* Cargo */}
+        <AgentItemArea className="col-2">
+          <AgentItemText>{agents.role}</AgentItemText>
+        </AgentItemArea>
 
-          {/* Unidade */}
-          <AgentItemArea className="col-2">
-            <AgentItemText>{agents.branch}</AgentItemText>
-          </AgentItemArea>
+        {/* Unidade */}
+        <AgentItemArea className="col-2">
+          <AgentItemText>{agents.branch}</AgentItemText>
+        </AgentItemArea>
 
-          {/* Status */}
-          <AgentItemArea className="col-2">
-            <AgentStatus>
-              <AgentStatusText>{agents.status}</AgentStatusText>
-            </AgentStatus>
-          </AgentItemArea>
+        {/* Status */}
+        <AgentItemArea className="col-2">
+          <AgentStatus>
+            <AgentStatusText>{agents.status}</AgentStatusText>
+          </AgentStatus>
+        </AgentItemArea>
 
-          {/* Opções */}
-          <AgentItemArea className="more col-1">
-            <AgentOptions>...</AgentOptions>
-          </AgentItemArea>
-        </AgentItems>
-      </Container>
+        {/* Opções */}
+        <AgentItemArea className="more col-1">
+          <AgentOptions>...</AgentOptions>
+        </AgentItemArea>
+      </AgentItems>
+    </Container>
   );
 };
--- components/CollaboratorCard/styles.ts
+++ components/CollaboratorCard/styles.ts
@@ -6,39 +6,51 @@ export const Container = styled.div`
   justify-content: center;
   height: 60px;
   width: 100%;
-  border-bottom: 1px solid #CCC;
- `;
+  border-bottom: 1px solid #ccc;
+`;
 export const AgentItems = styled.div`
   width: 100%;
   height: 100%;
   display: flex;
-`
+`;
 export const AgentAvatar = styled.img`
   width: 30px;
   height: 30px;
   border-radius: 15px;
   margin-right: 5px;
-`
+`;
 export const AgentMainArea = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-`
+  padding-left: 0;
+`;
+export const MainAreaLeftSide = styled.div`
+  width: 30%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-end;
+  align-items: center;
+`;
+export const MainAreaRightSide = styled.div`
+  width: 70%;
+  height: 100%;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+`;
 export const AgentItemArea = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  &.more{
+  &.more {
     justify-content: flex-end;
   }
-`
+`;
 export const AgentItemText = styled.span`
   font-family: Roboto;
   font-size: 14px;
-`
-export const AgentStatus = styled.div`
-`
-export const AgentStatusText = styled.span`
-`
-export const AgentOptions = styled.div`
-`
+`;
+export const AgentStatus = styled.div``;
+export const AgentStatusText = styled.span``;
+export const AgentOptions = styled.div``;
