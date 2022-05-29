8c4b8fc692862ec0d0d8758fa64e84354bfd1b2e
Leon Molina <leon.maguiar@gmail.com>
2022-05-29 16:14:57 -0300

    feat: role page basic structure

--- components/RoleBoard/index.tsx
+++ components/RoleBoard/index.tsx
@@ -1,30 +1,14 @@
 import Image from "next/image";
 import {
-  AgentAvatar,
   AgentCardSection,
-  AgentData,
-  AgentEmail,
-  AgentHeader,
   AgentInfo,
-  AgentInfoCard,
-  AgentName,
   AgentOrganizational,
   AgentOrgCard,
   Container,
-  InfoCardsArea,
-  InfoContent,
-  InfoData,
-  InfoIcon,
-  InfoText,
   InfoTitle,
-  OrgCardsArea,
   OrgCardText,
   OrgCardTitle,
-  OrgTitle,
 } from "./styles";
-import idIcon from "../../public/id.svg";
-import phoneIcon from "../../public/phone-call.svg";
-import dateIcon from "../../public/calendar.svg";
 import downIcon from "../../public/chevron-down.svg";
 import { Role } from "../../types/Role";
 
@@ -35,101 +19,49 @@ type Props = {
 export const RoleBoard = ({ role }: Props) => {
   return (
     <Container>
-      <AgentHeader>
-        <AgentAvatar>
-        </AgentAvatar>
-        <AgentData>
-          <AgentName>{role.department}</AgentName>
-          <AgentEmail>{role.name}</AgentEmail>
-        </AgentData>
-      </AgentHeader>
-
       <AgentInfo>
-        <InfoTitle>Informações pessoais</InfoTitle>
-
-        <InfoCardsArea>
-          <AgentInfoCard>
-            <InfoIcon>
-              <Image src={idIcon} width={18} height={18} alt={"Ícone simbolizando um documento."} />
-            </InfoIcon>
-            <InfoData>
-              <InfoText>{role.department}</InfoText>
-              <InfoContent>{role.department}</InfoContent>
-            </InfoData>
-          </AgentInfoCard>
-
-          <AgentInfoCard>
-            <InfoIcon>
-              <Image src={phoneIcon} width={18} height={18} alt={"Ícone simbolizando um telefone."} />
-            </InfoIcon>
-            <InfoData>
-              <InfoText>Telefone</InfoText>
-              <InfoContent>{role.name}</InfoContent>
-            </InfoData>
-          </AgentInfoCard>
-
-          <AgentInfoCard>
-            <InfoIcon>
-              <Image src={dateIcon} width={18} height={18} alt={"Ícone simbolizando um calendário."} />
-            </InfoIcon>
-            <InfoData>
-              <InfoText>Nascimento</InfoText>
-              <InfoContent>{role.department}</InfoContent>
-            </InfoData>
-          </AgentInfoCard>
-        </InfoCardsArea>
+        <InfoTitle>Dados do cargo</InfoTitle>
       </AgentInfo>
 
+      {/* Dados do cargo */}
       <AgentOrganizational>
-        <OrgTitle>Dados organizacionais</OrgTitle>
+        <AgentOrgCard>
+          <AgentCardSection>
+            <OrgCardTitle>Departamento</OrgCardTitle>
+            <OrgCardText>{role.department}</OrgCardText>
+          </AgentCardSection>
+          <AgentCardSection>
+            <Image
+              src={downIcon}
+              width={24}
+              height={24}
+              alt={
+                "Ícone de flecha apontando para baixo, indicando que há opções."
+              }
+            />
+          </AgentCardSection>
+        </AgentOrgCard>
 
-        <OrgCardsArea>
-
-            <AgentOrgCard>
-              <AgentCardSection>
-              <OrgCardTitle>Departamento</OrgCardTitle>
-              <OrgCardText>{role.department}</OrgCardText>
-              </AgentCardSection>
-              <AgentCardSection>
-                <Image src={downIcon} width={24} height={24} alt={"Ícone de flecha apontando para baixo, indicando que há opções."} />
-              </AgentCardSection>
-            </AgentOrgCard>
-
-            <AgentOrgCard>
-              <AgentCardSection>
-                <OrgCardTitle>Cargo</OrgCardTitle>
-                <OrgCardText>{role.name}</OrgCardText>
-              </AgentCardSection>
-              <AgentCardSection>
-                <Image src={downIcon} width={24} height={24} alt={"Ícone de flecha apontando para baixo, indicando que há opções."} />
-              </AgentCardSection>
-            </AgentOrgCard>
-
-            <AgentOrgCard>
-              <AgentCardSection>
-                <OrgCardTitle>Unidade</OrgCardTitle>
-                <OrgCardText>{role.name}</OrgCardText>
-              </AgentCardSection>
-              <AgentCardSection>
-                <Image src={downIcon} width={24} height={24} alt={"Ícone de flecha apontando para baixo, indicando que há opções."} />
-              </AgentCardSection>
-            </AgentOrgCard>
-
-            <AgentOrgCard>
-              <AgentCardSection>
-                <OrgCardTitle>Status</OrgCardTitle>
-                <OrgCardText>
-                  {role.department}
-                </OrgCardText>
-              </AgentCardSection>
-              <AgentCardSection>
-                <Image src={downIcon} width={24} height={24} alt={"Ícone de flecha apontando para baixo, indicando que há opções."} />
-              </AgentCardSection>
-            </AgentOrgCard>
-
-
-        </OrgCardsArea>
+        <AgentOrgCard>
+          <AgentCardSection>
+            <OrgCardTitle>Cargo</OrgCardTitle>
+            <OrgCardText>{role.name}</OrgCardText>
+          </AgentCardSection>
+          <AgentCardSection>
+            <Image
+              src={downIcon}
+              width={24}
+              height={24}
+              alt={
+                "Ícone de flecha apontando para baixo, indicando que há opções."
+              }
+            />
+          </AgentCardSection>
+        </AgentOrgCard>
       </AgentOrganizational>
+
+      {/* Área do board */}
+    
     </Container>
   );
 };
